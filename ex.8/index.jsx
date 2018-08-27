f341a1300d88a8435edde2d60ab3620cef49a464
Guilherme Zanini Moreira <guilherme.zm@outlook.com>
2018-08-27 19:43:27 +0000

    Passando propriedades para componente filho - 1

---
+++ ex.8/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Family from './family'
+import Member from './member'
+
+ReactDOM.render(
+    
+    <Family lastName="Moreira">
+        <Member name="Guilherme"/>
+        {/* React.cloneElement não aceita novos membros */}
+    </Family>
+
+,document.getElementById('app'))
